ccafa307e8659391a54f61b6566bd083302956d7
Robin Kim <therobinkim@gmail.com>
2015-12-20 05:01:48 -0600

    [Solving] Finish count, first attempt

--- app/count.js
+++ app/count.js
@@ -2,6 +2,27 @@ exports = (typeof window === 'undefined') ? global : window;
 
 exports.countAnswers =  {
   count : function (start, end) {
+    var obj = {
+      timers: []
+    };
 
+    console.log(start);
+    for(var i = start + 1; i <= end; i++) {
+      obj.timers.push(asyncLog(i, (i - start) * 100));
+    }
+
+    obj.cancel = function() {
+      obj.timers.forEach(function(timer) {
+        clearTimeout(timer);
+      });
+    };
+
+    return obj;
+
+    function asyncLog(data, delay) {
+      return setTimeout(function() {
+        console.log(data);
+      }, delay);
+    }
   }
 };
